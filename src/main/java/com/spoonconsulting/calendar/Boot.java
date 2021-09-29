c1d2a70c9fcb4490da503c59c78efde07790b0d3
abdool091 <abdool.miskin@sc-mauritius.com>
2021-09-29 21:03:55 +0400

    remove warning

--- src/main/java/com/spoonconsulting/calendar/Boot.java
+++ src/main/java/com/spoonconsulting/calendar/Boot.java
@@ -2,23 +2,21 @@ package com.spoonconsulting.calendar;
 
 import static jsweet.dom.Globals.setTimeout;
 import static jsweet.util.Globals.function;
-import static jsweet.lang.Globals.eval;
 
 import framework.components.api.ContainerRenderer;
 
 public class Boot {
 public static void main(String[] args) {
 
-		eval("window.calendar = com.spoonconsulting.calendar;");
 		
-		/*WeekView table =new WeekView("wv");
+		WeekView table =new WeekView("wv");
 		table.reset();
 	    setTimeout(function((e)->{
 	    	table.render(ContainerRenderer.getElementById("semainetype"));
 	    	//table.addItem("09:30", "12:30", "dispo", "Mercredi");
 	    	//table.setSemaineType(Util.getSemaineType());
 	    	table.render(ContainerRenderer.getElementById("semainetype"));
-	    }), 1000 );*/
+	    }), 1000 );
 	}
 
 }
